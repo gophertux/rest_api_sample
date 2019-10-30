03a02b16e6a8adb4b2697ee5d4d2769867897bd7
joseph <joseph@epiphytelcl.int.asurion.com>
2019-10-30 17:46:10 -0500

    added error handling for bad filter syntax and queries

--- sdc_sample/api.py
+++ sdc_sample/api.py
@@ -9,8 +9,11 @@ import middleware
 @app.route('/v0/launchpad_info')
 def return_launchpad_info():
     launchpad_info = middleware.launchpad_info(request.args)
+    url = request.url
 
-    if launchpad_info.status_code != 200:
+    if launchpad_info.status_code == 400:
+        return json.dumps({"msg": f"Your request {url} was unsuccessful. Please check your query and try again"})
+    elif launchpad_info.status_code != 200:
         return json.dumps({"msg": "internal server error - please check the logs"}), 500
 
     return json.dumps(launchpad_info.site_stats), 200 
