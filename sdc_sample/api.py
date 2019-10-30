3fb90dc07e636283a111dbca6c216b2e638f2aed
joseph <joseph@epiphytelcl.int.asurion.com>
2019-10-30 12:24:54 -0500

    added simple error handling for bad responses from the third party API

--- sdc_sample/api.py
+++ sdc_sample/api.py
@@ -1,20 +1,21 @@
-#TODO: Create route launchpad_info
-#TODO: Create obj exposing required attrs
 #TODO: error handling between middleware and front-end
-#TODO: README and requirements file
-
-#launchpad_id = id; launchpad_name = full_name; launchpad_status = status
-#TODO: logging, db
 
 from flask import Flask
 app = Flask(__name__)
 from flask import request 
 
-import middleware
 import json
+import logging
+import middleware
 
 @app.route('/v0/launchpad_info')
 def return_launchpad_info():
-    response = middleware.launchpad_info() #middleware.retrieve_launchpad_info(request.args.get('filter', ''))
-    
-    return json.dumps(response.site_stats)
+    launchpad_info = middleware.launchpad_info() #middleware.retrieve_launchpad_info(request.args.get('filter', ''))
+
+    if launchpad_info.status_code != 200:
+        return json.dumps({"msg": "internal server error - please check the logs"}), 500
+    #logging.info(json.dumps({
+    #    "middleware_response": lp_info.site_stats
+    #}))
+
+    return json.dumps(launchpad_info.site_stats), 200 
